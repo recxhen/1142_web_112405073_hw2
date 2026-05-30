dd88050b2e143d45f2ecab4c9087025ba0cbe9c2
recxhen <rexchen0611@gmail.com>
2026-05-30 22:53:48 +0800

    update

--- component/ActionButton.tsx
+++ component/ActionButton.tsx
@@ -1,17 +1,12 @@
 "use client"
 import Link from "next/link";
 
-export default function ActionButton(children: React.ReactNode){
+export default function ActionButton(){
   
 
   return(
     <>
-      <Link className="text-white bg-black px-3 py-2" href="/">children</Link>  
+      <Link className="text-white bg-black px-3 py-2" href="/">按鈕元件</Link>  
     </>
   )
-
-
-
-
-
 }
